ffffe312a55575b5bdec392e607b439e0dfff299
Ravi Prajapati <pmravi1997@gmail.com>
2022-04-06 11:16:23 +0530

    first commit

---
+++ practice.py
@@ -0,0 +1,51 @@
+# take a input and display it in reverse order
+# a = input("enter a string")
+# b = ''
+# for i in a:
+#     b = i+b
+
+# print(b)
+
+# find a value present in given list
+c = [1, 5, 4, 37, 6, 3, 2, 67, 86, 42, 6, 21]
+
+
+# def findfunc(find):
+#     for index, value in enumerate(c):
+#         if (value == find):
+#             return index
+#     return -1
+#     # print(value, index)
+
+
+# print(findfunc(37))
+
+# length of string
+# d = input("enter a string")
+# print(len(d))
+
+
+# return true if vowels present in string else false
+# e = input("enter a string")
+
+
+# def findVowel(str):
+#     for char in str:
+#         if char in "aeiou":
+#             return True
+#     return False
+
+
+# print(findVowel(e))
+
+
+# foot to inches
+foot = input("enter foot value : ")
+
+
+def footToInches(foot):
+    inches = 12
+    return int(foot) * inches
+
+
+print(footToInches(foot))
